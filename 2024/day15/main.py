6e9c696ab72114da1fa53ef01a093e057becba5f
WarrenWu4 <warrenweiwu04@gmail.com>
2024-12-18 13:05:04 -0600

    completed day 15

--- 2024/day15/main.py
+++ 2024/day15/main.py
@@ -1,6 +1,7 @@
 import time
+import copy
 
-with open ('c.in', 'r') as f:
+with open ('i.in', 'r') as f:
     data = f.read().strip('\n').split('\n\n')
     map = [list(l) for l in data[0].split('\n')]
     moves = "".join(data[1].split('\n'))
@@ -8,33 +9,12 @@ directions = {'^': (0, -1), 'v': (0, 1), '<': (-1, 0), '>': (1, 0)}
 
 def calculate_gps(map):
     gps = 0
+    valid_boxes = ['O', '[']
     for r in range(len(map)):
         for c in range(len(map[0])):
-            if (map[r][c] == 'O'):
+            if (map[r][c] in valid_boxes):
                 gps += 100 * r + c
     return gps
-
-def move_robot(robot, moves, map):
-    for move in moves:
-        og_position = (robot[0], robot[1])
-        can_move = False
-        (dx, dy) = directions[move]
-        while (map[robot[0]][robot[1]] != '#'):
-            if (map[robot[0]][robot[1]] == '.'):
-                can_move = True
-                break
-            robot = (robot[0] + dy, robot[1] + dx)
-        if (not can_move):
-            robot = (og_position[0], og_position[1])
-        while (can_move):
-            if (map[robot[0]][robot[1]] == '.'):
-                map[robot[0]][robot[1]] = 'O'
-            elif (map[robot[0]][robot[1]] == '@'):
-                map[robot[0]][robot[1]] = '.'
-                map[robot[0]+dy][robot[1]+dx] = '@'
-                og_position = robot = (robot[0]+dy, robot[1]+dx)
-                break
-            robot = (robot[0] - dy, robot[1] - dx)
 
 def print_map(map):
     for r in range(len(map)):
@@ -42,22 +22,130 @@ def print_map(map):
             print(map[r][c], end='')
         print()
 
-def part_a(map, moves):
-    robot_pos = (-1, -1)
+def get_robot_pos(map):
     for r in range(len(map)):
         for c in range(len(map[0])):
             if (map[r][c] == '@'):
-                robot_pos = (r, c)
-                break
-        if (robot_pos[0] != -1):
+                return (r, c)
+
+def update_map(map):
+    new_map = []
+    for r in range(len(map)):
+        new_map.append([])
+        for c in range(len(map[0])):
+            block = map[r][c]
+            if (block == '#'):
+                new_map[r].extend(list('##'))
+            elif (block == 'O'):
+                new_map[r].extend(list('[]'))
+            elif (block == '.'):
+                new_map[r].extend(list('..'))
+            else:
+                new_map[r].extend(list('@.'))
+    return new_map
+
+def horizontal_move(pos, move, map):
+    # check if can move horizontally
+    (dy, dx) = directions[move]
+    (x, y) = (pos[0]+dx, pos[1]+dy)
+    is_moveable = False
+    while (map[x][y] != '#'):
+        if (map[x][y] == '.'):
+            is_moveable = True
             break
-    # moves = '<^^>'
-    move_robot(robot_pos, moves, map)
-    print_map(map)
+        y += dy
+    # if yes then update map and return robot
+    if (is_moveable):
+        while(map[x][y] != '@'):
+            map[x][y] = map[x][y-dy]
+            y -= dy
+        map[x][y] = '.'
+        return (x, y+dy)
+    else:
+        return pos
+
+def get_boxes(pos, move, map, boxes):
+    (dy, dx) = directions[move]
+    (x, y) = (pos[0], pos[1])
+    if (map[x][y] == '#' or map[x][y] == '.'):
+        return
+    if (map[x][y] == ']'):
+        boxes.append((x,y-1))
+        get_boxes((x+dx, y-1+dy), move, map, boxes)
+    elif (map[x][y] == '['):
+        boxes.append((x,y+1))
+        get_boxes((x+dx, y+1+dy), move, map, boxes)
+    boxes.append((x,y))
+    get_boxes((x+dx, y), move, map, boxes)
+
+def sort_boxes(boxes):
+    # sort by x values
+    for i in range(len(boxes)):
+        for j in range(i+1, len(boxes)):
+          if (boxes[j][0] < boxes[i][0]):
+            temp = boxes[j]
+            boxes[j] = boxes[i]
+            boxes[i] = temp
+def reverse_sort_boxes(boxes):
+    for i in range(len(boxes)):
+        for j in range(i+1, len(boxes)):
+          if (boxes[j][0] > boxes[i][0]):
+            temp = boxes[j]
+            boxes[j] = boxes[i]
+            boxes[i] = temp
+
+def vertical_move(pos, move, map):
+    (dy, dx) = directions[move]
+    (x, y) = (pos[0]+dx, pos[1]+dy)
+    boxes = [pos]
+    get_boxes((x, y), move, map, boxes)
+    for (x, y) in boxes:
+          if (map[x+dx][y] == '#'):
+            return pos
+    boxes = list(set(boxes))
+    if (dx == 1):
+          reverse_sort_boxes(boxes)
+    else:
+          sort_boxes(boxes)
+    for (x, y) in boxes:
+        map[x+dx][y] = map[x][y]
+        map[x][y] = '.'
+    return (x+dx, y)
+
+def part_a(map, moves):
+    robot_pos = get_robot_pos(map)
+    for move in moves:
+        if (move == '<' or move == '>'):
+            robot_pos = horizontal_move(robot_pos, move, map)
+        else:
+            robot_pos = vertical_move(robot_pos, move, map)
+    ans = calculate_gps(map)
+    print(ans)
+
+def part_b(map, moves):
+    map = update_map(map)
+    robot_pos = get_robot_pos(map)
+    # move_num = 0
+    # moves = moves[:23]
+    for move in moves:
+        if (move == '<' or move == '>'):
+            robot_pos = horizontal_move(robot_pos, move, map)
+        else:
+            robot_pos = vertical_move(robot_pos, move, map)
+        """
+        move_num += 1
+        print(f'Move Number: {move_num}, {move}')
+        print_map(map)
+        """
     ans = calculate_gps(map)
     print(ans)
 
 a_s = time.time()
-part_a(map, moves)
+part_a(copy.deepcopy(map), moves)
 a_e = time.time()
 print(f'part a completed in {a_e-a_s} seconds')
+
+b_s = time.time()
+part_b(copy.deepcopy(map), moves)
+b_e = time.time()
+print(f'part b completed in {b_e-b_s} seconds')
